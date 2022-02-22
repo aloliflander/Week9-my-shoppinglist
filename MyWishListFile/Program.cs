0ef235591ab8098f3e84c3e31da407bc9396d93e
alo liflander <alo.liflander@gmail.com>
2022-02-22 11:32:39 +0200

    control flow

---
+++ MyWishListFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyWishListFIle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\Alo\Samples\Wishlist";
+            string fileName = @"//Mywishlist.txt";
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myWishList = arrayFromFile.ToList<string>();
+            bool loopActive = true;
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+                if(userInput== 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myWishList.Add(userWish);
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+            Console.Clear();
+            foreach (string wish in myWishList)
+            {
+                Console.WriteLine($"Your wish is {wish}");
+            }
+            File.WriteAllLines($"{fileLocation}{fileName}",myWishList);
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\Alo\Samples";
+            Console.WriteLine("Enter directory name.");
+            string newDirectory = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter file name.");
+            string fileName = Console.ReadLine().ToLower();
+            if (Directory.Exists($"{rootDirectory}//{newDirectory}") && File.Exists($"{rootDirectory}//{newDirectory}//{fileName}"))
+            {
+                Console.WriteLine("Directory and file allready exist.");
+                Console.WriteLine("Would you like to add a wish?");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}//{newDirectory}");
+                File.Create($"{rootDirectory}//{newDirectory}//{fileName}");
+            }
+        }
+    }
+}
